8132e27f00182d7b1852f18110f3b65b0d6e29da
Jan Holub <Jan-Holub@gmx.net>
2018-03-02 13:45:56 +0100

    Added the possibility to provide search tags

--- plugins/pr0gramm.py
+++ plugins/pr0gramm.py
@@ -6,6 +6,11 @@ import asyncio
 
 import metamodule
 
+# pro0gramm Plugin
+#
+# Searches for random images on pro0gramm form the popupar section
+# An arbitrary amount of search tags can be provided
+#
 class pr0gramm(metamodule.Meta):
 
     # command to call the plugin
@@ -15,31 +20,49 @@ class pr0gramm(metamodule.Meta):
     # sends help message
     async def help(self, message):
         help_str = """ Gets a random image or video from **pr0gramm.com**
+                - `!%s <tag1> ... <tagn>`: Random image or video with the provided tags attached
                 - `!%s`: Random image or video
-        """ % self.get_command()
+        """ % tuple([self.get_command()]*2)
         await self.client.send_message(message.channel, help_str)
 
     # main functionality of the plugin
     async def execute(self, command, message):
         # no command provided get some random pr0gramm image
-        if not command:
-            res = requests.get('http://pr0gramm.com/api/items/get', {
-                # SFW
-                'flags': 1,
-                'promoted': 1,
-            })
-            # check if the request was ok
-            if res.status_code == 200:
-                # parse the items
-                items = res.json()["items"]
-                # get a random item
-                random_item = items[random.randint(0, len(items))]
-                # info message
-                await self.client.send_message(message.channel, 'Here\'s what I found:')
-                # send the url to the channel
-                await self.client.send_message(message.channel, self._generateUrl(random_item))
-            else:
-                await self.client.send_message(message.channel, 'I couldn\'t reach **pro0gramm.com**')
+        random_item = self._getRandomItem(command)
+        # if we got something back from pr0gramm send it to the channel
+        if(random_item):
+            # info message
+            await self.client.send_message(message.channel, self._formatInfoMessage(command))
+            # send the url to the channel
+            await self.client.send_message(message.channel, self._generateUrl(random_item))
+        # if we couldn't get anything send an error message
+        else:
+            await self.client.send_message(message.channel, 'I couldn\'t reach **pro0gramm.com**')
+
+
+
+    # gets a random item from pr0gramm with the given tags attached
+    #
+    # @param tags - list of tags
+    #
+    # @return image object from the pr0gramm API or None if nothing was found
+    def _getRandomItem(self, tags=[]):
+        res = requests.get('http://pr0gramm.com/api/items/get', {
+            # SFW
+            'flags': 1,
+            'promoted': 1,
+            'tags': tags
+        })
+        # check if the request was ok
+        if res.status_code == 200:
+            # parse the items
+            items = res.json()["items"]
+            # get a random item
+            return items[random.randint(0, len(items))]
+        # if the response code was not 200 something went wrong
+        else:
+            return None
+
 
 
     # gererates the URL to the resource. It differs for videos and images
@@ -54,3 +77,30 @@ class pr0gramm(metamodule.Meta):
             return 'http://vid.pr0gramm.com/%s' % item["image"]
         else:
             return 'http://img.pr0gramm.com/%s' % item["image"]
+
+
+
+    # generates the info message for the bot response for the tags provided
+    # for the search
+    #
+    # @param tags - the search tags
+    #
+    # @return info message string
+    def _formatInfoMessage(self, tags):
+        # no search tags provided
+        if not tags:
+            return 'Here\'s what I found:'
+        # search tags provided
+        else:
+            # 2 tags or more
+            if len(tags) > 2:
+                message = 'Here\'s what I found for '
+                message += "".join(["**%s**, " % tag for tag in  tags[:len(tags)-2]])
+                message += "**%s** and **%s**:" % (tags[len(tags)-2], tags[len(tags)-1])
+                return message
+            # 2 tags
+            elif len(tags) == 2:
+                return 'Here\'s what I found for **%s** and **%s**:' % (tags[0], tags[1])
+            # 1 tag
+            else:
+                return 'Here\'s what I found for **%s**:' % tags[0]
