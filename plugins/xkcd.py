b9a4c9444448c4949cf65deac8a00b886ab6c6cb
Jan Holub <Jan-Holub@gmx.net>
2018-03-02 20:41:37 +0100

    Improved the xkcd plugin: specific xkcds can be fetched by providing the number

--- plugins/xkcd.py
+++ plugins/xkcd.py
@@ -15,13 +15,16 @@ class Xkcd(metamodule.Meta):
     async def help(self, message):
         helpstr = """ Gets comics from **xkcd.com**
                 - `!%s`: Get the latest xkcd comic
-        """ % self.get_command()
+                - `!%s <no>`: Get xkcd number <no>
+        """ % tuple([self.get_command()]*2)
         await self.client.send_message(message.channel, helpstr)
 
     # functionality
     async def execute(self, command, message):
         # no further parameters provided
+        # ===============
         # get latest xkcd
+        # ===============
         if not command:
             # get the latest comic
             response = requests.get('http://xkcd.com/info.0.json')
@@ -35,6 +38,37 @@ class Xkcd(metamodule.Meta):
             # 404 or anything else not 200
             else:
                 await self.client.send_message(message.channel, 'Sorry I coudn\'t reach **xkcd.com**')
+        # further parameters provied
+        # too many
+        elif len(command) > 1:
+            await self.client.send_message(message.channel, 'I\'m afraid you\'re using it wrong...')
+            await self.help(message)
+        # exactly one command
+        # =================================
+        # get xkcd with the number provided
+        # =================================
+        else:
+            # check if the parameter was a number
+            if command[0].isdigit():
+                # get the comic with the number provided
+                response = requests.get('http://xkcd.com/%s/info.0.json' % command[0])
+                if(response.status_code == 200):
+                    # parse the information
+                    comic_information = response.json()
+                    await self.client.send_message(message.channel, 'Here\'s the **xkcd** comic number **%s**:' % comic_information['num'])
+                    # XKCD embed
+                    comic_embed = self._createComicEmbed(comic_information)
+                    await self.client.send_message(message.channel, embed=comic_embed)
+                # not 200, might be a 404 for a non existant comic
+                else:
+                    await self.client.send_message(message.channel, 'Sorry but it seems that **xkcd** number **%s** has not been created yet...' % command[0])
+            # provied parameter was not a number
+            else:
+                await self.client.send_message(message.channel, 'Sorry but **%s** does not seem to be a number...' % command[0])
+                await self.help(message)
+
+
+
 
     # creates an Embed object containing all the information about the comic
     # image, date published, number, title and description
