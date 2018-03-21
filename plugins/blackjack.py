b0af644a64036b5ff2574be65780c6c54c43d789
Jan Holub <Jan-Holub@gmx.net>
2018-03-21 15:02:41 +0100

    [BlackJack] Player can draw cards and loses when they surpass 21 in value
    BlackJack is not yet recognized and the player can't stop and settle for a score yet

--- plugins/blackjack.py
+++ plugins/blackjack.py
@@ -11,9 +11,13 @@ class BlackJack(metamodule.Meta):
 
 
     def __init__(self, client):
-        # shuffeled card deck
+        # card deck
         self.deck = []
-        self._resetDeck()
+        # cards drawn by player
+        self.player_cards = []
+
+        # reset the game
+        self._resetGame()
 
         # call super contructor
         super(BlackJack, self).__init__(client)
@@ -24,20 +28,71 @@ class BlackJack(metamodule.Meta):
 
     # help message
     async def help(self, message):
-        helpstr = """ Play Backjack! Doesn't work yet..."""
+        helpstr = """ Play Backjack! Doesn't work yet...
+                - `!%s card`: Draw a new card
+                - `!%s reset`: Resets the game
+        """ % tuple([self.get_command()]*2)
         await self.client.send_message(message.channel, helpstr)
 
     # maximum number of allowed parameters
     def get_max_parameters(self):
-        return 0
+        return 1
 
     # functionality
     async def execute(self, command, message):
-        pass
+        # draw a new card
+        if command[0] == 'card':
+            self._drawCard()
+            card_list_string = "\n".join([card.getFullVerbose() for card in  self.player_cards])
+            await self.client.send_message(message.channel, card_list_string)
+            # reset the game if the cards surpass 21 in value
+            if(self._checkSurpassed21()):
+                await self.client.send_message(message.channel, "You lost!")
+                self._resetGame()
+        # reset the game
+        elif command[0] == 'reset':
+            self._resetGame()
+            await self.client.send_message(message.channel, "I resetted the game")
+        # wrong argument
+        else:
+            await self.client.send_message(message.channel, "I don't understand `%s`..." % command[0])
+            await self.help(message)
 
-    def _resetDeck(self):
+
+
+    # shuffles the deck (with all cards) and emptys the player_cards list
+    def _resetGame(self):
+        # shuffle a new deck of cards
         card_list = [Card(combination[0], combination[1]) for combination in itertools.product(range(2, 15),range(4))]
         self.deck = random.sample(card_list, len(card_list))
+        # empty player cards
+        self.player_cards = []
+
+
+
+    # draws a card from the deck and adds it to the player_cards list
+    #
+    # @return the card
+    def _drawCard(self):
+        card = self.deck.pop()
+        self.player_cards.append(card)
+        return card
+
+
+
+    # checks if the player has surpassed the value of 21
+    # Aces are treated as 1s
+    #
+    # @return true if >21 else false
+    def _checkSurpassed21(self):
+        # choose 1 as the value for Aces
+        return sum([1 if card.getVerboseValue() == 'Ace' else card.getGameValue() for card in self.player_cards]) > 21
+
+
+
+
+
+
 
 # playing card
 class Card:
@@ -47,13 +102,19 @@ class Card:
         self.value = value
         self.color = color
 
+
+
     # returns the cards color as a word
     def getVerboseColor(self):
         return ['Spades', 'Clubs', 'Hearts', 'Diamonds'][self.color]
 
+
+
     # returns the verbose value of a card
     def getVerboseValue(self):
         return ([str(x) for x in range(2, 11)] + ['Jack', 'Queen', 'King', 'Ace'])[self.value-2]
+
+
 
     # returns a string in the form of "card of color"
     # Examples:
@@ -61,7 +122,9 @@ class Card:
     #   Jack of Diamonds
     #   5 of Clubs
     def getFullVerbose(self):
-        return "%s of %s" % (self.getVerboseValue(), self.getVerboseColor())
+        return "**%s** of **%s**" % (self.getVerboseValue(), self.getVerboseColor())
+
+
 
     # returns the game value for the card
     #   Number of Number
